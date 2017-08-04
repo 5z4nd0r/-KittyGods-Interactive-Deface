209e542b1e7ed611eec91c14bf128d078c463e20
5z4nd0r <30715113+5z4nd0r@users.noreply.github.com>
2017-08-03 23:53:33 -0700

    Create index.php

---
+++ styles/n1n3t1es/index.php
@@ -0,0 +1,35 @@
+<!-- 
+# @KittyGods Interactive Deface
+# AUTHOR: ~[sz4n]~
+# STYLE: n1n3t1es
+# MIT License Protected
+ --->
+ 
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>c4tCORP waz h3r3</title>
+		<script> alert('h4x3d By c4tCORP')</script>
+		<script> alert('[!] ALERT: YOUVE B33N H4XD')</script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body bgcolor="black">
+		<center>
+		    <b><font face="Courier" color="#FFFFFF" size="6">[</font>
+			<font face="Courier" color="#00FF00" size="6"> u G0t H4CK3D by c4tCORP </font>
+			<font face="Courier" color="#FFFFFF" size="6">]</font></b>
+			<br>
+			<br>
+			<img src="http://orig14.deviantart.net/e2f3/f/2014/320/0/6/walking_color_palette__ascii_animated__by_junguler-d86ksee.gif" height="400px" width="400px">
+			<br>
+			<br>
+			<br>
+			<b><font face="Courier" color="#FFFFFF">Chances are if you're seeing this webpage , we have already destroyed everything we have found & this was what we decided to leave behind. :)</font>
+			<br>
+			<br>
+			<br>
+			<br>
+			<font face="Courier" color="#00FF00">If you'd like to get access back to your server/site contact us on Twitter @KittyGods ~[UrS3cSuX]~</face></b>
+		</center>
+	</body>
+</html>
